013a3681a63df9908c04a8d39d5385d9d385c587
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-08-05 13:09:52 +0530

    changed to hosted link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:  "https://server-todo-six.vercel.app/api",
+  baseURL: "https://server-todo-six.vercel.app/api",
 });
 
 export default instance;
